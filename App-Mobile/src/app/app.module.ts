91a0ad12b1c925629dbf7f0db96ede8d15592f80
Ben-Tu <tubulben@gmail.com>
2017-04-13 23:39:23 +0300

    --

--- App-Mobile/src/app/app.module.ts
+++ App-Mobile/src/app/app.module.ts
@@ -7,7 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import {MdButtonModule, MdCheckboxModule} from '@angular/material';
 import 'hammerjs';
-import { LoginComponent } from './Screens/login/login/login.component'
+import { LoginComponent } from './Screens/login/login.component'
 
 @NgModule({
   declarations: [
