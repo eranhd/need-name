b0f64f033da969c75344102c003132a6ae7edab8
Ben-Tu <tubulben@gmail.com>
2017-04-13 20:31:37 +0300

    update

--- appliction/src/app/app.module.ts
+++ appliction/src/app/app.module.ts
@@ -2,10 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MaterialModule} from '@angular/material';
+
 import { AppComponent } from './app.component';
-import 'hammerjs';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -13,13 +12,9 @@ import 'hammerjs';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
-    MdCheckboxModule,
-    rowserAnimationsModule,
-    MaerialModule.forRoot()
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-
 export class AppModule { }
