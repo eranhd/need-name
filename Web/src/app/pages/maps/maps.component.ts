53f387e1f08d019851e97346393b037f21ab9b3b
eranhd <eranm22@gmail.com>
2017-04-14 08:59:41 +0300

    markeup multiplie point in maps

--- Web/src/app/pages/maps/maps.component.ts
+++ Web/src/app/pages/maps/maps.component.ts
@@ -1,46 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { MapsService } from '../../service/maps/maps.service';
-import { SebmGoogleMap, SebmGoogleMapPolygon, LatLngLiteral } from 'angular2-google-maps/core';
+import { SebmGoogleMap, SebmGoogleMapPolygon, LatLngLiteral , SebmGoogleMapMarker} from 'angular2-google-maps/core';
+import { UserService } from '../../service/user/user.service';
 @Component({
   selector: 'app-maps',
   templateUrl: './maps.component.html',
   styleUrls: ['./maps.component.css']
 })
 export class MapsComponent implements OnInit {
-  private title: string = 'My first angular2-google-maps project';
   /*private lat:number;
   private lng:number;*/
 
   lat: number = 0;
   lng: number = 0;
+  lat2: number = 0;
+  lng2: number = 0;
   zoom: number = 10;
-  paths: Array<LatLngLiteral> = [
-    { lat: 0,  lng: 10 },
-    { lat: 0,  lng: 20 },
-    { lat: 10, lng: 20 },
-    { lat: 10, lng: 10 },
-    { lat: 0,  lng: 10 }
-  ]
-  // Nesting paths will create a hole where they overlap;
-  nestedPaths: Array<Array<LatLngLiteral>> = [[
-    { lat: 0,  lng: 10 },
-    { lat: 0,  lng: 20 },
-    { lat: 10, lng: 20 },
-    { lat: 10, lng: 10 },
-    { lat: 0,  lng: 10 }
-  ], [
-    { lat: 0, lng: 15 },
-    { lat: 0, lng: 20 },
-    { lat: 5, lng: 20 },
-    { lat: 5, lng: 15 },
-    { lat: 0, lng: 15 }
-  ]];
-  constructor(maps:MapsService) { 
+
+  constructor(maps:MapsService, 
+              private userService:UserService) { 
+
     var that = this;
     navigator.geolocation.getCurrentPosition(function(position){
       that.lat = position.coords.latitude;
       that.lng = position.coords.longitude;
+      
+      that.lat2 = position.coords.latitude + 1;
+      that.lng2 = position.coords.longitude + 1;
     });
+
+    
   
     
   }
