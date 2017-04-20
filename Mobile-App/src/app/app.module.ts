868670c703783c66f0fe398d0c592cfdef9db069
Ben-Tu <tubulben@gmail.com>
2017-04-21 00:33:07 +0300

    new app

--- Mobile-App/src/app/app.module.ts
+++ Mobile-App/src/app/app.module.ts
@@ -1,38 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
-
-import {MdButtonModule, MdCheckboxModule,MaterialModule} from '@angular/material';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { AngularFireModule } from 'angularfire2';
-import { AppComponent } from './app.component';
-import { LoginComponent } from './screens/login/login.component';
-import 'hammerjs';
 
-export const firebaseConfig = {
-  apiKey: "AIzaSyBzTCOzgKlkX-8vxhzDDFdH_M5QKRW53gw",
-    authDomain: "antidrugsjerusalem.firebaseapp.com",
-    databaseURL: "https://antidrugsjerusalem.firebaseio.com",
-    storageBucket: "antidrugsjerusalem.appspot.com",
-    messagingSenderId: "767801084656"
-};
+import { AppComponent } from './app.component';
 
 @NgModule({
-
   declarations: [
-    AppComponent,
-    LoginComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    MdButtonModule,
-    MdCheckboxModule,
-    MaterialModule.forRoot(),
-    AngularFireModule.initializeApp(firebaseConfig),
     FormsModule,
     HttpModule
   ],
-   exports: [MdButtonModule, MdCheckboxModule],
   providers: [],
   bootstrap: [AppComponent]
 })
