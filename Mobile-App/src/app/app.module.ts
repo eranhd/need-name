04f343ef937f87f81086eaecd8be36793767e359
Ben-Tu <tubulben@gmail.com>
2017-04-20 20:37:24 +0300

    work with material

--- Mobile-App/src/app/app.module.ts
+++ Mobile-App/src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {MdButtonModule, MdCheckboxModule,MaterialModule} from '@angular/material';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { AngularFireModule } from 'angularfire2';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './screens/login/login.component';
+import 'hammerjs';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBzTCOzgKlkX-8vxhzDDFdH_M5QKRW53gw",
+    authDomain: "antidrugsjerusalem.firebaseapp.com",
+    databaseURL: "https://antidrugsjerusalem.firebaseio.com",
+    storageBucket: "antidrugsjerusalem.appspot.com",
+    messagingSenderId: "767801084656"
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
+    MdButtonModule,
+    MdCheckboxModule,
+    MaterialModule.forRoot(),
+    AngularFireModule.initializeApp(firebaseConfig),
     FormsModule,
     HttpModule
   ],
