0b2f91795dbaa7d333d93df5634b173ef08a641d
Ben-Tu <tubulben@gmail.com>
2017-04-22 16:36:01 +0300

    working with ionic

--- Mobile-App/src/app/app.module.ts
+++ Mobile-App/src/app/app.module.ts
@@ -1,32 +1,40 @@
+import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { AngularFireModule } from 'angularfire2';
-import { AppComponent } from './app.component';
-import { LoginComponent } from './screens/login/login.component';
-import { MaterialModule } from '@angular/material';
-import {MdButtonModule, MdCheckboxModule} from '@angular/material';
-import {} from 'jasmine';
-import 'hammerjs';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
 
+import { AboutPage } from '../pages/about/about';
+import { ContactPage } from '../pages/contact/contact';
+import { HomePage } from '../pages/home/home';
+import { TabsPage } from '../pages/tabs/tabs';
 
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent
+    MyApp,
+    AboutPage,
+    ContactPage,
+    HomePage,
+    TabsPage
   ],
   imports: [
     BrowserModule,
-    MdButtonModule,
-    MdCheckboxModule,
-    MaterialModule.forRoot(),
-   //AngularFireModule.initializeApp(firebaseConfig),
-    FormsModule,
-    HttpModule
+    IonicModule.forRoot(MyApp)
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    AboutPage,
+    ContactPage,
+    HomePage,
+    TabsPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
 })
-export class AppModule { }
+export class AppModule {}
