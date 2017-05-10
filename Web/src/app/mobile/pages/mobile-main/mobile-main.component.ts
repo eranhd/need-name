aae94e26175f666e2c2b291c932b70a1bb5a1f9e
elitehadaroz <elitehadaroz@gmail.com>
2017-05-10 22:18:46 +0300

    changing name to spot page

--- Web/src/app/mobile/pages/mobile-main/mobile-main.component.ts
+++ Web/src/app/mobile/pages/mobile-main/mobile-main.component.ts
@@ -17,7 +17,7 @@ export class MobileMainComponent implements OnInit {
   }
 
   public buttonAddSpot(){
-    this.router.navigate(['mobile_spot']);
+    this.router.navigate(['mobile-spot']);
   }
 
   ngOnInit() {
