94a69a88bd1aa76b563b91988e2a7beb2f2bf245
Ben-Tu <tubulben@gmail.com>
2017-05-13 23:26:47 +0300

    add report to start partol

--- Web/src/app/mobile/pages/start-patrol/start-patrol.component.ts
+++ Web/src/app/mobile/pages/start-patrol/start-patrol.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-start-patrol',
@@ -7,9 +8,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StartPatrolComponent implements OnInit {
 
-  constructor() { }
+  public leader:string;
+  public numOfMembers:number;
+  public teamMember:string[]; //arry of volunteers name
+ // public numOfVolunteers:number;
+
+  constructor(public router:Router) {
+    this.numOfMembers=1;
+   // this.teamMember=[];
+  }
+
+
+  public addVolunteers(){
+    this.numOfMembers = parseInt((<HTMLInputElement>document.getElementById('numOfVolunteers')).value);
+    console.log(this.numOfMembers);
+   // var newMember ='<button type="button" class="btn-deleteMember btn-danger" (click)="deleteMember()"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></button><input _ngcontent-c1 type="text" class="volunteerName" id="volunteerName'+this.numOfMembers+'" placeholder="'+this.numOfMembers+' מתנדב"><br>';
+   // document.getElementById('volunteers').innerHTML += newMember;
+      for(var i = 0; i < this.numOfMembers-1; i++){
+          var newMember ='<input _ngcontent-c1 type="text" class="form-group" id="volunteerName'+(i+2)+'" placeholder="'+(i+2)+' מתנדב"><br>';
+           document.getElementById('volunteers').innerHTML += newMember;
+     }
+    };
+   
+  private deleteMember(){
+
+  };
+
+  private startPatrol(){
+   //  for(var i = 0; i < this.numOfMembers; i++){
+
+   //  }
+  };
 
   ngOnInit() {
+    console.log((<HTMLInputElement>document.getElementById('numOfVolunteers')).value);
   }
 
 }
+
---
+++ Web/src/app/models/start-patrol.ts
@@ -0,0 +1,18 @@
+
+
+export class startPatrol {
+
+    private team: number;
+    public date: string;
+    public time: string;
+    public teamLeader: string;
+    public location: string;
+    public loc: Location;
+    public numOfVolunteers:number;
+    public nameOfVolunteers:string[];
+
+    constructor(date: Date, teamLeader: string, numOfVolunteers:number , nameOfVolunteers:string[] ) {
+
+
+    }
+}
