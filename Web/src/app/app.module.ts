a7e8903da25f3a14fbaf4d979b66c3eea201b791
eran <eranm22@gmail.com>
2017-05-21 20:57:51 +0300

    add team statistic component

--- Web/src/app/app.module.ts
+++ Web/src/app/app.module.ts
@@ -48,6 +48,7 @@ import { MobileFooterComponent } from './mobile/pages/mobile-footer/mobile-foote
 import { MobileHomeComponent } from './mobile/pages/mobile-home/mobile-home.component';
 import { LastShiftComponent } from './pages/home/last-shift/last-shift.component';
 import { EndPatrolComponent } from './mobile/pages/end-patrol/end-patrol.component';
+import { TeamStatisticsComponent } from './mobile/pages/mobile-home/team-statistics/team-statistics.component';
 
 
 export const firebaseConfig = {
@@ -92,7 +93,8 @@ const myFirebaseAuthConfig = {
     MobileFooterComponent,
     MobileHomeComponent,
     LastShiftComponent,
-    EndPatrolComponent
+    EndPatrolComponent,
+    TeamStatisticsComponent
     
   ],
   imports: [
--- Web/src/app/app.router.ts
+++ Web/src/app/app.router.ts
@@ -14,10 +14,11 @@ import { GraphsComponent } from './pages/graphs/graphs/graphs.component';
 import { MobileLoginComponent } from './mobile/pages/mobile-login/mobile-login.component';
 
 import { StartPatrolComponent } from './mobile/pages/start-patrol/start-patrol.component'
-import {EndPatrolComponent} from './mobile/pages//end-patrol/end-patrol.component';
+import { EndPatrolComponent } from './mobile/pages//end-patrol/end-patrol.component';
 
 import { MobileSpotComponent } from './mobile/pages/mobile-spot/mobile-spot.component';
 import { MobileHomeComponent } from './mobile/pages/mobile-home/mobile-home.component';
+import { TeamStatisticsComponent } from './mobile/pages/mobile-home/team-statistics/team-statistics.component';
 
 export const router: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full' },
@@ -38,12 +39,18 @@ export const router: Routes = [
     {
         path: 'mobile_main', component: MobileMainComponent,
         children: [
-            { path: '', redirectTo: 'mobile_home', pathMatch: 'full' },
+            { path: '', redirectTo: 'mobile_home', pathMatch: 'full', },
             { path: 'report', component: ReportComponent },
             { path: 'start_patrol', component: StartPatrolComponent },
             { path: 'mobile_spot', component: MobileSpotComponent },
-            { path: 'mobile_home' , component: MobileHomeComponent},
-            {path:  'end_patrol', component: EndPatrolComponent}
+            {
+                path: 'mobile_home', component: MobileHomeComponent,
+                children: [
+                    { path: '', redirectTo: 'team-statistics', pathMatch: 'full' },
+                    { path: 'team-statistics', component: TeamStatisticsComponent }
+                ]
+            },
+            { path: 'end_patrol', component: EndPatrolComponent }
         ]
     },
 
