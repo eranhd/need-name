9dfb359e54cf8b85986f36ebed23cdbec35b6a34
eran <eranm22@gmail.com>
2017-05-21 19:00:31 +0300

    add back button in app #27

--- Web/src/app/mobile/pages/mobile-header/mobile-header.component.ts
+++ Web/src/app/mobile/pages/mobile-header/mobile-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-mobile-header',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MobileHeaderComponent implements OnInit {
 
+  @Input() showBack: boolean;
   constructor() { }
 
   ngOnInit() {
