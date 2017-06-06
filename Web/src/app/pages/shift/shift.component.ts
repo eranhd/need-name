c75b1f2fb576f1d24ac257f6c103141c9f859326
Eran Hadad <eranm22@gmail.com>
2017-06-06 15:59:43 +0300

    search in shift component design and ngModel

--- Web/src/app/pages/shift/shift.component.ts
+++ Web/src/app/pages/shift/shift.component.ts
@@ -9,9 +9,32 @@ import { FirebaseService } from '../../service/firebase/firebase.service';
 })
 export class ShiftComponent implements OnInit {
 
-  
-  constructor(public firebseService: FirebaseService) { }
+  numData: string[];
+  leadName: string;
 
+  locationName: string;
+
+  fromDate: Date;
+  toDate: Date;
+
+  fromReportNum: string = "0";
+  toReportNum: string = "0";
+
+  fromHotNum: string = "0";
+  toHotNum: string = "0";
+
+  fromColdNum: string = "0";
+  toColdNum: string = "0";
+
+  constructor(public firebseService: FirebaseService) { 
+    this.numData = [];
+    for(let i = 1; i < 20; i++)
+      this.numData.push(i+'');
+  }
+
+  search(){
+    console.log(this.fromReportNum)
+  }
   ngOnInit() {
     
   }
