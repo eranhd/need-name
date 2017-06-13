401a860628d6479f3d6e1c275992e55b33cf1e33
elite <elitehadaroz@gmail.com>
2017-06-13 14:22:31 +0300

    table show shift

--- Web/src/app/pages/show-report/show-report.component.ts
+++ Web/src/app/pages/show-report/show-report.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, Input } from "@angular/core";
 import { Report } from "../../models/Report";
 
 @Component({
-  selector: "app-show-report",
+  selector: "show-report",
   templateUrl: "./show-report.component.html",
   styleUrls: ["./show-report.component.css"]
 })
@@ -14,13 +14,7 @@ export class ShowReportComponent implements OnInit {
   }
 
   ngOnInit() {
-      console.log("test")
-      console.log(this.report);
-      firebase.storage().ref().child(this.report.photoUrl).getDownloadURL().then(url => {
-      this.test = url;
-    }).catch(error => {
-      console.log(error.message);
-    });
+      
   }
 
 }
